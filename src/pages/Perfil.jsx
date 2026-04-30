decda8314ddc3d7617e35b2d9d98a4b92c00101c
PedroRegas <pedro.regas@gmail.com>
2026-04-30 08:55:29 -0300

    Add files via upload

---
+++ src/pages/Perfil.jsx
@@ -0,0 +1,24 @@
+(imports)
+import { useContext } from 'react';
+import { UserContext } from '../App';
+import Card from 'react-bootstrap/Card';
+
+function Perfil() {
+	const { username } = useContext(UserContext);
+	return (
+		<Card style={{ maxWidth: 400, margin: '2rem auto', textAlign: 'center' }}>
+			<Card.Body>
+				<Card.Title>Perfil</Card.Title>
+				<Card.Text>
+					{username ? (
+						<>Bem-vindo, <b>{username}</b>!</>
+					) : (
+						<>Nenhum nome informado.</>
+					)}
+				</Card.Text>
+			</Card.Body>
+		</Card>
+	);
+}
+
+export default Perfil;
